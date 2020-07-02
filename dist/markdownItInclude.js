f59a03b218bd129ef8eed113eccad2ce5ab367cc
Ger Hobbelt <ger@hobbelt.com>
2020-07-03 00:06:23 +0200

    - updated the README documentation of the options & API - all tests pass, 100% coverage - bumped patch level to signal feature set enhancement

--- dist/markdownItInclude.js
+++ dist/markdownItInclude.js
@@ -1,4 +1,4 @@
-/*! markdown-it-include 1.1.1-5 https://github.com//GerHobbelt/markdown-it-include @license MIT */
+/*! markdown-it-include 1.1.2-6 https://github.com//GerHobbelt/markdown-it-include @license MIT */
 
 'use strict';
 
